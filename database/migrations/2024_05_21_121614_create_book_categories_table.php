78247c1cf4207f2bad39b344b21318906320022b
doidoi1294 <h520524b@mails.cc.ehime-u.ac.jp>
2024-06-28 07:44:34 +0900

    Procfile for Heroku2

--- database/migrations/2024_05_21_121614_create_book_categories_table.php
+++ database/migrations/2024_05_21_121614_create_book_categories_table.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('book_categories');
+        // Schema::dropIfExists('book_categories');
     }
 };
